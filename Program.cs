8d57c6c0bb51b0e226f3e3505b3886b0be7ad347
DeSnowMan <112641348+DeSnowMan@users.noreply.github.com>
2023-01-11 09:42:16 -0700

    asgsgagasg

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Distance Calculator De Jeffary
+Console.Clear();
+Console.WriteLine("Bienvenida, Mundo!");
+
+//reciber input de la usario
+Console.WriteLine("escribe la valor primero de X");
+double X = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("escribe la valor primero de Y");
+double Y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("escribe la valor segundo de X");
+double X1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("escribe la valor segundo de Y");
+double Y1 = Convert.ToDouble(Console.ReadLine());
+
+// procesar los informacciones de anteriormente
+double XS = Math.Pow((X1 - X), 2);
+
+double YS = Math.Pow((Y1 - Y), 2);
+
+// Calcular el solucione
+double Answer =  Math.Sqrt( XS + YS );
+Console.WriteLine($"El solucione es: {Answer}");
+          
